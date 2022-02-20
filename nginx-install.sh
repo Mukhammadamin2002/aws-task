94e3f9bf9cf3a549d8d47e95cd5ae5f5e8b635ff
Abdullaev <mukhammadaminabdullaev01082002@gmail.com>
2022-02-20 22:00:08 +0500

    update/script

--- nginx-install.sh
+++ nginx-install.sh
@@ -6,4 +6,5 @@ echo "Nginx installed successfully!"
 sudo chown $USER /var/www/html
 touch /var/www/html/index.html
 echo "Hello World" > /var/www/html/index.html
+
 uname -a >> /var/www/html/index.html
