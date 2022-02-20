ee2f5ac0c67984a4f9574b806b5d498277c1874b
Abdullaev <mukhammadaminabdullaev01082002@gmail.com>
2022-02-20 17:23:42 +0500

    update/script

--- nginx-install.sh
+++ nginx-install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 password=$1
 echo -e $password | sudo -S apt install nginx -y 
+sudo systemctl start nginx.service
 echo "Nginx installed successfully!"
 sudo chown $USER /var/www/html
 touch /var/www/html/index.html
 echo "Hello World" > /var/www/html/index.html
-
 uname -a >> /var/www/html/index.html
